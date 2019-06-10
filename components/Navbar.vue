85e55eb49278f4832559809461c2fecc36dac3e0
oleg <perchykoleg@gmail.com>
2019-06-09 22:07:06 -0500

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+        <a href="#" class="logo text-lg">
+            Navbar
+        </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .nav {
+        height: 70px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+        box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
+        padding: 0 24px 0 24px;
+    }
+
+</style>
