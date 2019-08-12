162fa58a954e78c246aa8f26de690ee85779aee7
oleg <perchykoleg@gmail.com>
2019-08-12 18:30:24 -0500

    updated router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,60 @@
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+const ghPages = {
   router: {
-    base: '/nuxt-githubpages/'
+    base: "/nuxt-githubpages/"
   }
-} : {}
+};
+
+const routerBase = process.env.DEPLOY_ENV === "GH_PAGES" ? ghPages : "";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   router: {
     base: routerBase
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap' }
-    
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#CA5FA6' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#CA5FA6" },
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/scss/main.scss'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/scss/main.scss"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma',
+    "@nuxtjs/bulma"
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     postcss: {
       preset: {
@@ -59,9 +64,8 @@ export default {
       }
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
